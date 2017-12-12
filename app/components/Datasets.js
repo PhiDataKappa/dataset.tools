a60f0e6f597659d1765c8897b4421c53aa5ac9e1
Ben Collins <ben@Bens-MacBook-Air.local>
2017-12-12 09:44:56 -0600

    datasets render on datasets tab

--- app/components/Datasets.js
+++ app/components/Datasets.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 export default class Datasets extends Component {
   constructor(props){
     super(props);
+    console.log(props, 'props in dataset')
+    console.log(this.props, 'this.props in dataset')
   }
  render() {
+   var hasUserData = Array.isArray(this.props.userData) ? this.props.userData : [];
    return (
+     <div>
+     <Table onRowSelection={this.handleRowSelection}>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>This Won't Show</TableHeaderColumn>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Size</TableHeaderColumn>
+            <TableHeaderColumn>Download</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          { hasUserData.map((project, index) =>
+              project.files.map((file, index2) =>
+
+
+            <TableRow>
+            <TableRowColumn></TableRowColumn>
+            <TableRowColumn>{file.name}</TableRowColumn>
+            <TableRowColumn>{(file.sizeInBytes/1000)} kb</TableRowColumn>
+            <TableRowColumn>Download</TableRowColumn>
+          </TableRow>
+            )
+          )}
+
+        </TableBody>
+      </Table>
      <p>Hi im in Datasets</p>
+   </div>
    )
  }
 }
