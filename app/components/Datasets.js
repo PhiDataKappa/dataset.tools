2404e391b027134c44eeaa73cfe039e4193f481e
Ben Collins <ben@Bens-MacBook-Air.local>
2017-12-14 15:46:12 -0600

    click handler for download file

--- app/components/Datasets.js
+++ app/components/Datasets.js
@@ -7,21 +7,28 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
 export default class Datasets extends Component {
   constructor(props){
     super(props);
     console.log('props in dataset',props)
     console.log('this.props in dataset',this.props)
   }
+
+ getFile(name) {
+   console.log('getting file' + name);
+ }
+
  render() {
    var hasUserData = Array.isArray(this.props.userData) ? this.props.userData : [];
    var tableStyles = {
      height: '500px',
      overflowY: 'auto'
    }
+   var clicked = () => {console.log('clicked')}
    return (
      <div className='datasetTable' style={tableStyles}>
-     <Table onRowSelection={this.handleRowSelection} style="overflow-y:auto">
+     <Table onRowSelection={this.handleRowSelection}>
         <TableHeader>
           <TableRow>
             <TableHeaderColumn>This Won't Show</TableHeaderColumn>
@@ -37,9 +44,9 @@ export default class Datasets extends Component {
 
             <TableRow>
             <TableRowColumn></TableRowColumn>
-            <TableRowColumn>{file.name}</TableRowColumn>
+            <TableRowColumn><RaisedButton onClick={() => {console.log('clicked')}}>Download</RaisedButton></TableRowColumn>
             <TableRowColumn>{(file.sizeInBytes/1000)} kb</TableRowColumn>
-            <TableRowColumn>Download</TableRowColumn>
+            <TableRowColumn><RaisedButton onClick={() => this.getFile(file.name)}>Download</RaisedButton></TableRowColumn>
           </TableRow>
             )
           )}
