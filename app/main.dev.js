18a5670429ee5c7ed895bdab30e2e93669a1623b
noblemillie <noble.millie@gmail.com>
2017-12-14 14:10:27 -0600

    added event listeners in main.dev.js

--- app/main.dev.js
+++ app/main.dev.js
@@ -10,66 +10,102 @@
  *
  * @flow
  */
-import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import url from 'url';
+import {app, crashReporter, BrowserWindow, Menu} from 'electron';
+// import {app, BrowserWindow, Menu} from 'electron';
+// import { app, BrowserWindow } from 'electron';
 import MenuBuilder from './menu';
 
-let mainWindow = null;
+const isDevelopment = (process.env.NODE_ENV === 'development');
+const isProduction = (process.env.NODE_ENV === 'production');
 
-if (process.env.NODE_ENV === 'production') {
+let mainWindow = null;
+let forceQuit = false;
+
+
+if (isProduction) {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
 }
 
-if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+if (isDevelopment || process.env.DEBUG_PROD === 'true') {
   require('electron-debug')();
-  const path = require('path');
+  // const path = require('path');
   const p = path.join(__dirname, '..', 'app', 'node_modules');
   require('module').globalPaths.push(p);
 }
 
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
   const extensions = [
     'REACT_DEVELOPER_TOOLS',
     'REDUX_DEVTOOLS'
   ];
-
-  return Promise
-    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-    .catch(console.log);
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  for (const name of extensions) {
+    try {
+      await installer.default(installer[name], forceDownload);
+    } catch (e) {
+      console.log(`Error installing ${name} extension: ${e.message}`);
+    }
+  }
 };
+
+// crashReporter.start({
+//   productName: 'YourName',
+//   companyName: 'YourCompany',
+//   submitURL: 'https://data.tools/url-to-submit',
+//   uploadToServer: false
+// });
+
 
 
 /**
- * Add event listeners...
+ * ========================= Add event listeners...
  */
 
 app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 
 app.on('ready', async () => {
-  if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+  if (isDevelopment || process.env.DEBUG_PROD === 'true') {
     await installExtensions();
   }
 
   mainWindow = new BrowserWindow({
     show: false,
     width: 1024,
+    minWidth: 640,
+    minHeight: 480,
     height: 738
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
+  // mainWindow.loadURL(`file://${__dirname}/app.html`);
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'app.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // show window once on first load
+  mainWindow.webContents.once('did-finish-load', () => {
+    mainWindow.show();
+  });
+
+
 
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
   mainWindow.webContents.on('did-finish-load', () => {
+    // Handle window logic properly on macOS:
+    // 1. App should not terminate if window has been closed
+    // 2. Click on icon in dock should re-open the window
+    // 3. ⌘+Q should close the window and quit the app
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
@@ -77,10 +113,41 @@ app.on('ready', async () => {
     mainWindow.focus();
   });
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+  if (process.platform === 'darwin') {
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+
+    app.on('activate', () => {
+      mainWindow.show();
+      mainWindow.focus();
+    });
+
+    app.on('before-quit', () => {
+      forceQuit = true;
+    });
+  } else {
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+  }
 
   const menuBuilder = new MenuBuilder(mainWindow);
   menuBuilder.buildMenu();
+
+
+  if (isDevelopment) {
+    // auto-open dev tools
+    mainWindow.webContents.openDevTools();
+
+    // add inspect element on right click menu
+    mainWindow.webContents.on('context-menu', (e, props) => {
+      Menu.buildFromTemplate([{
+        label: 'Inspect element',
+        click() {
+          mainWindow.inspectElement(props.x, props.y);
+        }
+      }]).popup(mainWindow);
+    });
+  }
 });
