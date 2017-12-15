7e223d758ad8101cdf0e77296809024561bf51b9
Joshua Pawlik <joshawesome12@yahoo.com>
2017-12-15 11:05:29 -0600

    Update Home.js

--- app/components/Home.js
+++ app/components/Home.js
@@ -16,13 +16,14 @@ export default class Home extends Component {
       fireRedirect:false
     }
   }
-
+  
   componentDidMount() {
-    if (document.cookie){
+    if (document.cookie && this.props.shouldRedirect){
       var cookie = document.cookie;
       cookie = cookie.split('=')[1];
       console.log(cookie);
       this.props.addAccessToken(cookie);
+      this.props.setShouldRedirect(true);
       this.setState({fireRedirect:true})
     }
     //if token
