0d20ed5f74d0816c57d988a08b3902091c2897e3
noblemillie <noble.millie@gmail.com>
2017-12-15 14:48:22 -0600

    refactored 'dataworld_www_redirect.js' for greater reusability

--- app/dataworld_www_redirect.js
+++ app/dataworld_www_redirect.js
@@ -1,8 +1,22 @@
 const shell = require('electron').shell
 
-const exLinksBtnDataWorld = document.getElementById('open-ex-links-dataworld')
+const openExLinksInBrowser = document.getElementById('open-ex-links-dataworld')
 
-//opens the data.world website in a new tab in chrome.
-exLinksBtnDataWorld.addEventListener('click', function (event) {
+//opens an external link (i.e. data.world) website in browser.
+openExLinksInBrowser.addEventListener('click', function (event) {
   shell.openExternal('http://data.world')   // NOTE: concat username to end of URL (i.e. http://data.world/user) to go straight to users homepage
 })
+
+
+//NOTE: if multiple external links added (i.e. 'http://dataset.tools') use the following block instead of openExLinksInBrowser.
+// const links = document.querySelectorAll('a[href]')
+//
+// Array.prototype.forEach.call(links, function (link) {
+//   const url = link.getAttribute('href')
+//   if (url.indexOf('http') === 0) {
+//     link.addEventListener('click', function (e) {
+//       e.preventDefault()
+//       shell.openExternal(url)
+//     })
+//   }
+// })
