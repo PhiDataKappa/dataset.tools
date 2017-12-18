1bf5fc69f6464d6fdc383c10ff27a58d5159ac43
Ben Collins <ben@Bens-MacBook-Air.local>
2017-12-18 17:14:55 -0600

    renders datasets properly

--- app/containers/Datasets.js
+++ app/containers/Datasets.js
@@ -2,13 +2,19 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Datasets from '../components/Datasets';
 import * as DatasetsActions from '../actions/Datasets';
+import * as ProjectsActions from '../actions/Projects';
 
 function mapStateToProps(state) {
   return state;
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(DatasetsActions, dispatch);
+  return {
+    actions: {
+      DatasetsActions: bindActionCreators(DatasetsActions, dispatch),
+      ProjectsActions: bindActionCreators(ProjectsActions, dispatch)
+    }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Datasets);
