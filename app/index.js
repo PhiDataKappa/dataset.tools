ba5e61f498946d06e9fe9119d0bb0dacba423b8a
noblemillie <noble.millie@gmail.com>
2017-12-19 10:39:42 -0600

    context menu

--- app/index.js
+++ app/index.js
@@ -32,3 +32,15 @@ if (module.hot) {
     );
   });
 }
+
+const {ipcRenderer} = require('electron')
+
+document.addEventListener('DOMContentLoaded', () => {
+  let n = new Notification('dataset.tools ready!', {
+    body: 'Tooling up.'
+  })
+
+  // Tell the notification to show the menubar popup window on click
+  n.onclick = () => { ipcRenderer.send('show-window') }
+
+})
